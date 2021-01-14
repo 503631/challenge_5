2e5428e38611f7789b4ac0d0c191ddf436a97fb4
Even70000 <75841107+Even70000@users.noreply.github.com>
2021-01-14 10:13:36 +0100

    ik heb de invoeg menu verandert naar FAQ

--- invoegen.php
+++ invoegen.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,7 +8,6 @@
     <link rel="stylesheet" href="css/style.css" />
     <title>Document</title>
 </head>
-
 <body>
     <!-- #region Navbar-->
     <div id="navbar"></div>
@@ -38,4 +36,3 @@
     <!-- #endregion -->
 </body>
 </html>
-
