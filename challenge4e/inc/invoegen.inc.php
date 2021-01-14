8121d603892e39e728dd2b613066388685fd256f
Stefan Bollen <503631@vistacollege.nl>
2021-01-14 09:39:52 +0100

    oke

--- challenge4e/inc/invoegen.inc.php
+++ challenge4e/inc/invoegen.inc.php
@@ -1,10 +1,10 @@
 <div class="contactform">
         <h1 style="color:darkcyan;">stuur een bericht</h1>
         <form action="./upload.php" method="post" >
-            <input type="text" name="name" placeholder="naam gerecht"><br />
-            <input type="text" name="prijs" placeholder="prijs producht"><br />
+            <input type="text" name="name" placeholder="name of dish"><br />
+            <input type="text" name="prijs" placeholder="price product"><br />
             <input type="file" name="fileToUpload" id="fileToUpload" ><br />
-            <textarea style="width:300px; height:150px; margin-top:10px;" name="message" placeholder="bericht">schrijf jou bericht hier in:
+            <textarea style="width:300px; height:150px; margin-top:10px;" name="message" placeholder="bericht">write your message here:
             </textarea><br />
             <a href=""><input type="submit" value="Upload Image" value="stuur"></a>
         </form>
