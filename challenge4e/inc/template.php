cf87441e2f10a8c2963a1838a5292c1a41301a58
Even70000 <75841107+Even70000@users.noreply.github.com>
2021-01-15 09:53:00 +0100

    button verandert

--- challenge4e/inc/template.php
+++ challenge4e/inc/template.php
@@ -1,3 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+</head>
+<body>
+    <?php 
+    $menu = <<<HTML
+    <form action="./shopping.php" method="post">
+    <div class="contentItem">
+        <div class="row">
+        <div  class="menutitle" name="id">$id</div>
+            <div  class="menutitle" name="titel">$titel</div>
+            <input hidden value="$titel" name="titel" ><br>
+            <div class="price" name="prijs">€$prijs</div>
+            <input hidden value="$prijs" name="prijs"><br>
+            <input hidden value="$id" name="id"><br>
+        </div>
+        <img src="$img">
+        <input type="number" min="0" name="aantal">
+        <input class="button" type="submit" value="buy">
+    </div>
+    </form>
+    
+    
+    HTML;
+    
+    echo $menu;
+    ?>
+</body>
+</html>
+
 <?php
 // $menu = <<<HTML
 
@@ -14,24 +49,4 @@
 
 // echo $menu;
 
-$menu = <<<HTML
-<form action="./shopping.php" method="post">
-<div class="contentItem">
-    <div class="row">
-    <div  class="menutitle" name="id">$id</div>
-        <div  class="menutitle" name="titel">$titel</div>
-        <input hidden value="$titel" name="titel" ><br>
-        <div class="price" name="prijs">€$prijs</div>
-        <input hidden value="$prijs" name="prijs"><br>
-        <input hidden value="$id" name="id"><br>
-    </div>
-    <img src="$img">
-    <input type="number" min="0" name="aantal">
-    <input type="submit" value="buy">
-</div>
-</form>
 
-
-HTML;
-
-echo $menu;
--- inc/template.php
+++ inc/template.php
@@ -1,3 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+</head>
+<body>
+<?php
+   $menu = <<<HTML
+   <form action="./shopping.php" method="post">
+   <div class="contentItem">
+     <div class="row">
+     <div  class="menutitle" name="id">$id</div>
+        <div  class="menutitle" name="titel">$titel</div>
+        <input hidden value="$titel" name="titel" ><br>
+        <div class="price" name="prijs">€$prijs</div>
+        <input hidden value="$prijs" name="prijs"><br>
+        <input hidden value="$id" name="id"><br>
+     </div>
+        <img src="$img">
+        <input type="number" min="0" name="aantal">
+        <input class="button" type="submit" value="Kopen">
+   </div>
+   </form>
+
+   HTML;
+
+   echo $menu;
+   ?>
+</body>
+</html>
+
 <?php
 // $menu = <<<HTML
 
@@ -14,24 +48,3 @@
 
 // echo $menu;
 
-$menu = <<<HTML
-<form action="./shopping.php" method="post">
-<div class="contentItem">
-    <div class="row">
-    <div  class="menutitle" name="id">$id</div>
-        <div  class="menutitle" name="titel">$titel</div>
-        <input hidden value="$titel" name="titel" ><br>
-        <div class="price" name="prijs">€$prijs</div>
-        <input hidden value="$prijs" name="prijs"><br>
-        <input hidden value="$id" name="id"><br>
-    </div>
-    <img src="$img">
-    <input type="number" min="0" name="aantal">
-    <input type="submit" value="Kopen">
-</div>
-</form>
-
-
-HTML;
-
-echo $menu;
